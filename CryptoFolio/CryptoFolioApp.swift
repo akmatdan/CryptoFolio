92d62981b059d0e9ef14a6279383090a6be8f0ae
Dan Akmat <akmatdan@gmail.com>
2022-12-09 14:08:14 +0600

    Initial Commit

---
+++ CryptoFolio/CryptoFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoFolioApp.swift
+//  CryptoFolio
+//
+//  Created by Daniil Akmatov on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
