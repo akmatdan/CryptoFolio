a8913275f5edca13f66883809973256d92201139
Dan Akmat <akmatdan@gmail.com>
2023-01-09 21:54:15 +0600

    Updates

---
+++ CryptoFolio/Utilities/LocalFileManager.swift
@@ -0,0 +1,21 @@
+//
+//  LocalFileManager.swift
+//  CryptoFolio
+//
+//  Created by Daniil Akmatov on 2/1/23.
+//
+
+import Foundation
+import SwiftUI
+
+class LocalFileManager {
+    
+    static let imstance = LocalFileManager()
+    private init() {  }
+    
+    func saveImage(image: UIImage) {
+        
+        let 
+    }
+    
+}
